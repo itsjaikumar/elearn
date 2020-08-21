ca0fb71083a1a9f3c185d1bd05f54c6ed0e4cb65
Jayakumar Reddy Rachamalla <66196161+itsjaikumar@users.noreply.github.com>
2020-08-21 20:48:44 +0530

    Update settings.py

--- class/settings.py
+++ class/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h9t)wxs!*c04!%7ug)v(ix&6c9t(%5)ad5gkuah_ex71lfy43@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['jaikumar-e-learn-app.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -38,15 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'teach',
-    'django_filters',
-    'storages',
+ 
 ]
 
 MIDDLEWARE = [
     
     'django.middleware.security.SecurityMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
